3efd09c85545b0eba98731ed267ce27c505e4bb8
MortyCPP <morty.cpp@gmail.com>
2024-12-12 14:50:34 +0800

    v1.0

---
+++ Wanwu/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnSendRequest;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnSendRequest = new System.Windows.Forms.Button();
+            this.txtShow = new System.Windows.Forms.TextBox();
+            this.txtAPIKey = new System.Windows.Forms.TextBox();
+            this.APITitle = new System.Windows.Forms.TextBox();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.APIKeylink = new System.Windows.Forms.LinkLabel();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtInput.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.txtInput.Location = new System.Drawing.Point(12, 12);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(340, 50);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnSendRequest
+            // 
+            this.btnSendRequest.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnSendRequest.Location = new System.Drawing.Point(12, 68);
+            this.btnSendRequest.Name = "btnSendRequest";
+            this.btnSendRequest.Size = new System.Drawing.Size(100, 30);
+            this.btnSendRequest.TabIndex = 1;
+            this.btnSendRequest.Text = "发送请求";
+            this.btnSendRequest.UseVisualStyleBackColor = true;
+            this.btnSendRequest.Click += new System.EventHandler(this.btnSendRequest_Click);
+            // 
+            // txtShow
+            // 
+            this.txtShow.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txtShow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtShow.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtShow.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.txtShow.Location = new System.Drawing.Point(12, 110);
+            this.txtShow.Multiline = true;
+            this.txtShow.Name = "txtShow";
+            this.txtShow.ReadOnly = true;
+            this.txtShow.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtShow.Size = new System.Drawing.Size(640, 240);
+            this.txtShow.TabIndex = 2;
+            this.txtShow.TabStop = false;
+            this.txtShow.Text = "响应将返回在此处";
+            // 
+            // txtAPIKey
+            // 
+            this.txtAPIKey.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAPIKey.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtAPIKey.Location = new System.Drawing.Point(418, 27);
+            this.txtAPIKey.Name = "txtAPIKey";
+            this.txtAPIKey.Size = new System.Drawing.Size(234, 23);
+            this.txtAPIKey.TabIndex = 3;
+            // 
+            // APITitle
+            // 
+            this.APITitle.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.APITitle.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.APITitle.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.APITitle.Location = new System.Drawing.Point(359, 29);
+            this.APITitle.Name = "APITitle";
+            this.APITitle.ReadOnly = true;
+            this.APITitle.Size = new System.Drawing.Size(53, 16);
+            this.APITitle.TabIndex = 4;
+            this.APITitle.TabStop = false;
+            this.APITitle.Text = "API Key：";
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnSettings.Location = new System.Drawing.Point(425, 68);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(85, 30);
+            this.btnSettings.TabIndex = 5;
+            this.btnSettings.Text = "设置API Key";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnReset.Location = new System.Drawing.Point(560, 68);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(85, 30);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "重置Key";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // APIKeylink
+            // 
+            this.APIKeylink.AutoSize = true;
+            this.APIKeylink.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.APIKeylink.Location = new System.Drawing.Point(552, 5);
+            this.APIKeylink.Name = "APIKeylink";
+            this.APIKeylink.Size = new System.Drawing.Size(100, 17);
+            this.APIKeylink.TabIndex = 7;
+            this.APIKeylink.TabStop = true;
+            this.APIKeylink.Text = "点击获取API Key";
+            this.APIKeylink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.APIKeylink_LinkClicked);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.BackColor = System.Drawing.Color.Transparent;
+            this.btnCopy.BackgroundImage = global::Wanwu.Properties.Resources.CopyImg;
+            this.btnCopy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnCopy.FlatAppearance.BorderSize = 0;
+            this.btnCopy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCopy.Font = new System.Drawing.Font("微软雅黑", 20F);
+            this.btnCopy.ForeColor = System.Drawing.Color.Black;
+            this.btnCopy.Location = new System.Drawing.Point(600, 112);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(32, 32);
+            this.btnCopy.TabIndex = 8;
+            this.btnCopy.TabStop = false;
+            this.btnCopy.UseVisualStyleBackColor = false;
+            this.btnCopy.Visible = false;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(664, 361);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.APIKeylink);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.APITitle);
+            this.Controls.Add(this.txtAPIKey);
+            this.Controls.Add(this.txtShow);
+            this.Controls.Add(this.btnSendRequest);
+            this.Controls.Add(this.txtInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "万物AI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox txtShow;
+        private System.Windows.Forms.TextBox txtAPIKey;
+        private System.Windows.Forms.TextBox APITitle;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.LinkLabel APIKeylink;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
---
+++ Wanwu/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Drawing;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        //API URL
+        private static readonly string apiUrl = "https://api.lingyiwanwu.com/v1/chat/completions";
+        private static string apiKey = "";
+
+        //消息列表
+        private static List<Dictionary<string, string>> messageHistory = new List<Dictionary<string, string>>();
+
+        //创建一个用于检测鼠标是否停留在btnCopy按钮上的Timer对象
+        private Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtInput.KeyPress += (sender, e) =>
+            {
+                if (e.KeyChar == (char)13) //判断是否按下回车键
+                {
+                    //禁止回车键产生换行
+                    e.Handled = true; //设置为true来阻止默认换行行为
+                    btnSendRequest.PerformClick(); //模拟按钮点击事件
+                }
+            };
+
+            txtAPIKey.KeyPress += (sender, e) =>
+            {
+                if (e.KeyChar == (char)13) //判断是否按下回车键
+                {
+                    //禁止回车键产生换行
+                    e.Handled = true; //设置为true来阻止默认换行行为
+                    btnSettings.PerformClick(); //模拟按钮点击事件
+                }
+            };
+
+            //订阅MouseMove事件
+            txtShow.MouseMove += txtShow_MouseMove;  
+
+            //初始化Timer设置为每隔100毫秒触发一次
+            timer = new Timer();
+            timer.Interval = 100;
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //将上次有效的APIKey初始化到窗口中
+            txtAPIKey.Text= Wanwu.Properties.Settings.Default.APIKey;
+        }
+
+        private void txtShow_MouseMove(object sender, MouseEventArgs e)
+        {
+            //检查鼠标是否停留在特定位置
+            if (e.X >= btnCopy.Location.X - txtShow.Location.X && e.X <= btnCopy.Location.X - txtShow.Location.X + btnCopy.Size.Width && e.Y >= btnCopy.Location.Y - txtShow.Location.Y && e.Y <= btnCopy.Location.Y - txtShow.Location.Y + btnCopy.Size.Height)
+            {
+                //显示按钮
+                btnCopy.Visible = true;
+                timer.Start(); //启动 Timer
+            }
+        }
+
+        //定时器触发时执行隐藏按钮
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+
+            //获取鼠标位置
+            Point mousePos = Control.MousePosition;
+            //将鼠标位置转换为按钮的客户端坐标
+            Point clientMousePos = btnCopy.PointToClient(mousePos);
+            //判断鼠标是否离开按钮区域
+            if (!btnCopy.ClientRectangle.Contains(clientMousePos))
+            {
+                //隐藏复制按钮
+                btnCopy.Visible = false;
+                timer.Stop();
+            }
+        }
+
+
+        //发送请求
+        private async void btnSendRequest_Click(object sender, EventArgs e)
+        {
+            //光标移动到输入框
+            txtInput.Focus();
+            //禁用按钮，防止连点
+            btnSendRequest.Enabled = false;
+            btnSettings.Enabled = false;
+            btnReset.Enabled = false;
+            txtShow.Text = "";
+            if (apiKey != "")
+            {
+                //获取文本框中的输入内容
+                string userInput = txtInput.Text;
+                if (userInput == "")
+                {
+                    txtShow.Text = "请输入内容";
+                }
+                else
+                {
+                    txtShow.Text = "正在获取响应，请稍后......";
+                    // 调用异步方法获取结果
+                    string result = await SendRequestAsync(userInput);
+                    // 显示返回结果
+                    txtShow.Text = result;
+                }
+            }
+            else
+            {
+                txtShow.Text = "请设置正确的API Key";
+            }
+            btnSendRequest.Enabled = true;
+            btnSettings.Enabled = true;
+            btnReset.Enabled = true;
+        }
+
+        //发送请求的方法
+        public static async Task<string> SendRequestAsync(string str)
+        {
+            //控制历史消息数量
+            if(messageHistory.Count >= 10)
+            {
+                messageHistory.RemoveRange(0, 2);
+            }
+            using (var client = new HttpClient())
+            {
+                //设置请求头
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+                //添加消息到历史消息列表
+                Dictionary<string, string> mesUser = new Dictionary<string, string>
+                    {
+                        { "role", "user" },
+                        { "content", str }
+                    };
+                messageHistory.Add(mesUser);
+
+                // 构建请求体
+                var jsonData = new
+                {
+                    model = "yi-large",
+                    messages = messageHistory,
+                    temperature = 0.3
+                };
+
+                //使用JsonConvert将对象转为JSON字符串
+                string jsonString = JsonConvert.SerializeObject(jsonData, Formatting.Indented);
+
+                //使用StringContent将JSON数据传入请求
+                var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                try
+                {
+                    //发送POST请求
+                    var response = await client.PostAsync(apiUrl, content);
+                    //确保请求成功
+                    response.EnsureSuccessStatusCode();
+                    //读取并输出响应内容
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    JObject responseObject = JObject.Parse(responseContent);
+                    //访问嵌套字段
+                    var messageContent = responseObject?["choices"]?.FirstOrDefault()?["message"]?["content"]?.ToString();
+                    if (messageContent != null)
+                    {
+                        Dictionary<string, string> mesAssistant = new Dictionary<string, string>
+                    {
+                        { "role", "assistant" },
+                        { "content", messageContent }
+                    };
+                        messageHistory.Add(mesAssistant);
+                        return messageContent;
+                    }
+                    else
+                    {
+                        return "没有有效内容返回......";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                    return "Error: " + ex.Message;
+                }
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtAPIKey.Text = "";
+            apiKey = "";
+            Wanwu.Properties.Settings.Default.APIKey = apiKey;
+            Wanwu.Properties.Settings.Default.Save();
+            txtShow.Text = "API Key重置成功";
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            //验证APIKey的正则表达式
+            string pattern = "^[a-z0-9]{32}$";
+            //创建Regex对象
+            Regex regex = new Regex(pattern);
+            //使用IsMatch方法检查输入字符串是否匹配
+            bool isMatch = regex.IsMatch(this.txtAPIKey.Text);
+            if (isMatch)
+            {
+                apiKey = this.txtAPIKey.Text.ToString();
+                Wanwu.Properties.Settings.Default.APIKey = apiKey;
+                Wanwu.Properties.Settings.Default.Save();
+                txtShow.Text = "API Key设置成功";
+            }
+            else
+            {
+                apiKey = "";
+                txtShow.Text = "请设置正确的API Key";
+            }
+        }
+
+        private void APIKeylink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string url = "https://platform.lingyiwanwu.com/";
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            //将展示框的内容复制到剪贴板
+            Clipboard.SetText(txtShow.Text);
+        }
+    }
+}
